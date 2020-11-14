e52cfd811b8eb49a6883eedb180849db15953fb5
Gautham J <Gauthamastro@gmail.com>
2020-11-14 10:16:40 +0530

    Modified RPC and WS ports

--- scripts/validator_bob.sh
+++ scripts/validator_bob.sh
@@ -2,9 +2,9 @@
   --base-path /tmp/bob \
   --chain customSpecRaw.json \
   --bob \
-  --port 30334 \
-  --ws-port 9945 \
-  --rpc-port 9934 \
+  --port 30333 \
+  --ws-port 9944 \
+  --rpc-port 9933 \
   --validator \
   --node-key 0000000000000000000000000000000000000000000000000000000000000002 \
   --bootnodes /ip4/54.183.253.27/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
--- scripts/validator_charlie.sh
+++ scripts/validator_charlie.sh
@@ -3,8 +3,8 @@
   --chain customSpecRaw.json \
   --charlie \
   --port 30334 \
-  --ws-port 9945 \
-  --rpc-port 9934 \
+  --ws-port 9944 \
+  --rpc-port 9933 \
   --validator \
   --node-key 0000000000000000000000000000000000000000000000000000000000000003 \
   --bootnodes /ip4/54.183.253.27/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
