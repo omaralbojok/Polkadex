f83ca6dcfff699dbcf42206520bb28874c8e4526
ksr30 <krishna.singh9926@gmail.com>
2021-01-06 17:21:49 +0000

    updated code

--- scripts/createCustomSpec.sh
+++ scripts/createCustomSpec.sh
@@ -1,2 +1,2 @@
-../target/release/node-polkadex build-spec --disable-default-bootnode --chain local > customSpec.json
-../target/release/node-polkadex build-spec --chain=customSpec.json --raw --disable-default-bootnode > customSpecRaw.json
+../target/release/polkadex-mainnet build-spec --disable-default-bootnode --chain local > customSpec.json
+../target/release/polkadex-mainnet build-spec --chain=customSpec.json --raw --disable-default-bootnode > customSpecRaw.json
