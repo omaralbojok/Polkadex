471b0cffcd7c0afebbb89afaddf8161a9af6ef47
ksr30 <krishna.singh9926@gmail.com>
2021-01-11 03:49:04 +0000

    updated ip

--- scripts/validator_bob.sh
+++ scripts/validator_bob.sh
@@ -8,4 +8,4 @@
   --rpc-port 9934 \
   --validator \
   --node-key 0000000000000000000000000000000000000000000000000000000000000002 \
-  --bootnodes /ip4/3.22.114.112/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
+  --bootnodes /ip4/18.217.225.182/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
--- scripts/validator_charlie.sh
+++ scripts/validator_charlie.sh
@@ -8,4 +8,4 @@
   --rpc-port 9934 \
   --validator \
   --node-key 0000000000000000000000000000000000000000000000000000000000000003 \
-  --bootnodes /ip4/3.22.114.112/tcp/30333/p2p/12D3KooWSCufgHzV4fCwRijfH2k3abrpAJxTKxEvN1FDuRXA2U9x
+  --bootnodes /ip4/18.217.225.182/tcp/30333/p2p/12D3KooWSCufgHzV4fCwRijfH2k3abrpAJxTKxEvN1FDuRXA2U9x
