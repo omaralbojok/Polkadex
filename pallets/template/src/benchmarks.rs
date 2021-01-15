cac6f82126b28dc316508e187bc48b0b7bee1194
ksr30 <krishna.singh9926@gmail.com>
2021-01-15 10:23:25 +0530

    added benchmark code

---
+++ pallets/template/src/benchmarks.rs
@@ -0,0 +1,17 @@
+#![cfg(feature = "runtime-benchmarks")]
+
+use super::*;
+use sp_std::prelude::*;
+use frame_system::RawOrigin;
+use frame_support::{ensure, traits::OnFinalize,};
+use frame_benchmarking::{benchmarks, TrackedStorageKey, account};
+use sp_core::H256;
+const SEED: u32 = 0;
+
+benchmarks! {
+    _ {}
+    do_something{
+        let accountid = account("caller", 0, SEED);
+
+    }:_(RawOrigin::Signed(accountid), 2)
+}
--- scripts/bootnode_alice.sh
+++
@@ -1,8 +0,0 @@
-../target/release/node-polkadex \
-  --base-path /tmp/alice \
-  --chain customSpecRaw.json \
-  --alice \
-  --port 30333 \
-  --ws-port 9944 \
-  --node-key 0000000000000000000000000000000000000000000000000000000000000001 \
-  --validator
